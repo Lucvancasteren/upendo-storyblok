becfa96f53c97566352fd1a2696595060c677f0a
Lucvancasteren <528507@student.fontys.nl>
2024-11-14 14:30:33 +0100

    navbar in main gepuhst
    navbar in main gepusht

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8f7f7060851236d"; }
+/******/ 		__webpack_require__.h = function() { return "92408e5e9320a94b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
