cf19d0cad23210964747f7e242037ffc87951f6a
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2024-11-29 14:13:48 +0100

    Calltoaction versie1
    omdat onze vorige repository fouten had en ik de calltoaction al had gemaakt, is hier de gehele eerste versie. In versie 2 wil ik hem meer responsive maken

--- components/StoryblokProvider.ts
+++ components/StoryblokProvider.ts
@@ -12,6 +12,7 @@ import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import AboutUs_hero from "./AboutUs_hero/AboutUs_hero";
 import ourValues from "./ourValues/ourValues";
+import CallToAction_1 from "./CallToAction_1/CallToAction_1";
 
 const components = {
   page: Page,
@@ -21,6 +22,7 @@ const components = {
   Footer: Footer,
   AboutUs_hero: AboutUs_hero,
   ourValues: ourValues,
+  CallToAction_1 : CallToAction_1,
 };
 
 storyblokInit({
