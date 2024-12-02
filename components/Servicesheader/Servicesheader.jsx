2846ccf6d229f9b2af00c79c458f7f10bc26db3a
freekvermaas <freekvermaas@gmail.com>
2024-12-02 11:53:45 +0100

    text fix
    tekst geen richtext meer, kleuren bijna helemaal aangepast

--- components/Servicesheader/Servicesheader.jsx
+++ components/Servicesheader/Servicesheader.jsx
@@ -1,18 +1,15 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
-import { render } from "storyblok-rich-text-react-renderer";
  
 const Servicesheader = ({ blok }) => (
   <div className="bg-[#002626]" {...storyblokEditable(blok)}>
     <div className="flex item-center justify-center">
         <div className="flex flex-col item-center justify-center">
-        {render(blok.Servicesheader_Titel1)}
-        {render(blok.Servicesheader_Titel2)}
-        {render(blok.Servicesheader_Titel3)}
+        <h3 className="text-[#F4C5FF]">{blok.Servicesheader_Titel1}</h3>
+        <h1 className="text-[#FFFFFF]">{blok.Servicesheader_Titel2}</h1>
+        <h3 className="text-[#EAFFBD]">{blok.Servicesheader_Titel3}</h3>
         </div>
-    
-    <img src={blok.Servicesheader_Image.filename} />
+    <img src={blok.Servicesheader_Image.filename}/>
     </div>
-    
   </div>
  
 );
