459cdca28a2bcfe64bc11fc19eb67053cfac5da1
freekvermaas <freekvermaas@gmail.com>
2024-12-04 17:08:30 +0100

    final woensdag changes

--- components/Servicesmain/Servicesmain.jsx
+++ components/Servicesmain/Servicesmain.jsx
@@ -4,7 +4,7 @@ const Servicesmain = ({ blok }) => (
   <div className="bg-[#002626] py-10 px-4" {...storyblokEditable(blok)}>
     <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl mx-auto">
       {/* Servicesmain 1 */}
-      <div className="text-center space-y-4">
+      <div className="bg-[#003A47] text-center space-y-4">
         <img src={blok.Servicesmain_Image1.filename} alt="Service Main 1" className="max-w-full h-auto"/>
         <h2 className="text-[#EAFFBD] text-lg">
           {blok.Servicesmain_Titel1}
@@ -12,7 +12,7 @@ const Servicesmain = ({ blok }) => (
         <p className="text-white text-sm">{blok.Servicesmain_Tekst1}</p>
       </div>
       {/* Servicesmain 2 */}
-      <div className="text-center space-y-4">
+      <div className="text-center space-y-4 my-10">
         <img src={blok.Servicesmain_Image2.filename} alt="Service Main 2" className="max-w-full h-auto"/>
         <h2 className="text-[#EAFFBD] text-lg">
           {blok.Servicesmain_Titel2}
@@ -28,7 +28,7 @@ const Servicesmain = ({ blok }) => (
         <p className="text-white text-sm">{blok.Servicesmain_Tekst3}</p>
       </div>
       {/* Servicesmain 4 */}
-      <div className="text-center space-y-4">
+      <div className="bg-[#003A47] text-center space-y-4 my-10">
         <img src={blok.Servicesmain_Image4.filename} alt="Service Main 4" className="max-w-full h-auto"/>
         <h2 className="text-[#EAFFBD] text-lg">
           {blok.Servicesmain_Titel4}
