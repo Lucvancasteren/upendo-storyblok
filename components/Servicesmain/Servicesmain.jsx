b0812465c6097ad3f4a632dc48f08d7edfa3afda
freekvermaas <freekvermaas@gmail.com>
2024-12-05 14:45:55 +0100

    servicesmain accurate positioning
    - mist nog buttons

--- components/Servicesmain/Servicesmain.jsx
+++ components/Servicesmain/Servicesmain.jsx
@@ -1,39 +1,57 @@
-import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
+import { storyblokEditable } from "@storyblok/react/rsc";
 
 const Servicesmain = ({ blok }) => (
   <div className="bg-[#002626] py-10 px-4" {...storyblokEditable(blok)}>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl mx-auto">
-      {/* Servicesmain 1 */}
-      <div className="bg-[#003A47] text-center space-y-4">
-        <img src={blok.Servicesmain_Image1.filename} alt="Service Main 1" className="max-w-full h-auto"/>
-        <h2 className="text-[#EAFFBD] text-lg">
-          {blok.Servicesmain_Titel1}
-        </h2>
-        <p className="text-white text-sm">{blok.Servicesmain_Tekst1}</p>
-      </div>
-      {/* Servicesmain 2 */}
-      <div className="text-center space-y-4 my-10">
-        <img src={blok.Servicesmain_Image2.filename} alt="Service Main 2" className="max-w-full h-auto"/>
-        <h2 className="text-[#EAFFBD] text-lg">
-          {blok.Servicesmain_Titel2}
-        </h2>
-        <p className="text-white text-sm">{blok.Servicesmain_Tekst2}</p>
-      </div>
-      {/* Servicesmain 3 */}
-      <div className="text-center space-y-4">
-        <img src={blok.Servicesmain_Image3.filename} alt="Service Main 3" className="max-w-full h-auto"/>
-        <h2 className="text-[#EAFFBD] text-lg">
-          {blok.Servicesmain_Titel3}
-        </h2>
-        <p className="text-white text-sm">{blok.Servicesmain_Tekst3}</p>
-      </div>
-      {/* Servicesmain 4 */}
-      <div className="bg-[#003A47] text-center space-y-4 my-10">
-        <img src={blok.Servicesmain_Image4.filename} alt="Service Main 4" className="max-w-full h-auto"/>
-        <h2 className="text-[#EAFFBD] text-lg">
-          {blok.Servicesmain_Titel4}
-        </h2>
-        <p className="text-white text-sm">{blok.Servicesmain_Tekst4}</p>
+    <div className="mx-auto max-w-[1220px] overflow-hidden px-6 py-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
+        {/* Servicesmain 1 */}
+        <div className="bg-[#003A47] -ml-96 flex flex-col justify-between text-left p-4 space-y-4">
+          <img
+            src={blok.Servicesmain_Image1.filename}
+            alt="Service Main 1"
+            className="max-w-full h-auto ml-96"
+          />
+          <div>
+            <h2 className="ml-96 text-[#EAFFBD] text-lg">{blok.Servicesmain_Titel1}</h2>
+            <p className="ml-96 text-white text-sm">{blok.Servicesmain_Tekst1}</p>
+          </div>
+        </div>
+        {/* Servicesmain 2 */}
+        <div className="flex flex-col justify-between text-left p-4 space-y-4">
+          <img
+            src={blok.Servicesmain_Image2.filename}
+            alt="Service Main 2"
+            className="max-w-full h-auto pt-32"
+          />
+          <div>
+            <h2 className="text-[#EAFFBD] text-lg">{blok.Servicesmain_Titel2}</h2>
+            <p className="text-white text-sm">{blok.Servicesmain_Tekst2}</p>
+          </div>
+        </div>
+        {/* Servicesmain 3 */}
+        <div className="flex flex-col justify-between text-left p-4 space-y-4">
+          <img
+            src={blok.Servicesmain_Image3.filename}
+            alt="Service Main 3"
+            className="max-w-full h-auto"
+          />
+          <div>
+            <h2 className="text-[#EAFFBD] text-lg">{blok.Servicesmain_Titel3}</h2>
+            <p className="text-white text-sm">{blok.Servicesmain_Tekst3}</p>
+          </div>
+        </div>
+        {/* Servicesmain 4 */}
+        <div className="bg-[#003A47] -mr-96 flex flex-col justify-between text-left p-4 space-y-4">
+          <img
+            src={blok.Servicesmain_Image4.filename}
+            alt="Service Main 4"
+            className="max-w-full h-auto mr-96"
+          />
+          <div>
+            <h2 className="text-[#EAFFBD] mr-96 text-lg">{blok.Servicesmain_Titel4}</h2>
+            <p className="text-white text-sm mr-96">{blok.Servicesmain_Tekst4}</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
