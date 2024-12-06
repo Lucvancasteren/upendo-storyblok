cd2cd654e515f0f147a62122a0f81be9b0c46a1c
freekvermaas <freekvermaas@gmail.com>
2024-12-06 15:19:37 +0100

    services merged with main

--- components/Servicesmain/Servicesmain.jsx
+++ components/Servicesmain/Servicesmain.jsx
@@ -1,39 +1,87 @@
-import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
+import { storyblokEditable } from "@storyblok/react/rsc";
 
 const Servicesmain = ({ blok }) => (
-  <div className="bg-[#002626] py-10 px-4" {...storyblokEditable(blok)}>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl mx-auto">
-      {/* Servicesmain 1 */}
-      <div className="bg-[#003A47] text-center space-y-4">
-        <img src={blok.Servicesmain_Image1.filename} alt="Service Main 1" className="max-w-full h-auto"/>
-        <h2 className="text-[#EAFFBD] text-lg">
-          {blok.Servicesmain_Titel1}
-        </h2>
-        <p className="text-white text-sm">{blok.Servicesmain_Tekst1}</p>
+  <div className="bg-[#002626] mt-8 mb-8" {...storyblokEditable(blok)}>
+    <div className="w-[100%]">
+      <div className="boven flex ">
+        {/*1*/}
+        <div className="w-[50%] bg-[#003a47]">
+          <div className="bg-[#003A47] flex flex-col justify-between text-left p-10 space-y-4">
+            <div className="flex justify-center">
+              <img src={blok.Servicesmain_Image1.filename} alt="Service Main 1" className="w-[72%] h-auto"/>
+            </div>
+            <div className=" w-[90%] flex flex-col items-center">
+              <div className="flex justify-start w-[70%]">
+                <h2 className=" text-[#EAFFBD] text-3xl mb-2 font-bold">{blok.Servicesmain_Titel1}</h2>
+              </div>
+                <div className="ml-2 w-[72%]">
+                  <p className=" text-white text-sm">{blok.Servicesmain_Tekst1}</p>
+                </div>
+                <div className="flex justify-start w-[70%] mt-4">
+                  <button className="bg-[#F4C5FF] px-6 py-2 rounded-2xl">Discover</button>
+                </div>
+            </div>
+          </div>
+        </div>
+        {/*2*/}
+        <div className="w-[50%] bg-[#002626]">
+          <div className="bg-[#002626] flex flex-col justify-between text-left p-10 space-y-4">
+            <div className="flex justify-center">
+              <img src={blok.Servicesmain_Image2.filename} alt="Service Main 1" className="w-[72%] h-auto"/>
+            </div>
+            <div className=" w-[90%] flex flex-col items-center">
+              <div className="flex justify-start w-[70%]">
+                <h2 className=" text-[#EAFFBD] text-3xl mb-2 font-bold">{blok.Servicesmain_Titel2}</h2>
+              </div>
+                <div className="ml-2 w-[72%]">
+                  <p className=" text-white text-sm">{blok.Servicesmain_Tekst2}</p>
+                </div>
+                <div className="flex justify-start w-[70%] mt-4">
+                  <button className="bg-[#F4C5FF] px-6 py-2 rounded-2xl">Discover</button>
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
-      {/* Servicesmain 2 */}
-      <div className="text-center space-y-4 my-10">
-        <img src={blok.Servicesmain_Image2.filename} alt="Service Main 2" className="max-w-full h-auto"/>
-        <h2 className="text-[#EAFFBD] text-lg">
-          {blok.Servicesmain_Titel2}
-        </h2>
-        <p className="text-white text-sm">{blok.Servicesmain_Tekst2}</p>
-      </div>
-      {/* Servicesmain 3 */}
-      <div className="text-center space-y-4">
-        <img src={blok.Servicesmain_Image3.filename} alt="Service Main 3" className="max-w-full h-auto"/>
-        <h2 className="text-[#EAFFBD] text-lg">
-          {blok.Servicesmain_Titel3}
-        </h2>
-        <p className="text-white text-sm">{blok.Servicesmain_Tekst3}</p>
-      </div>
-      {/* Servicesmain 4 */}
-      <div className="bg-[#003A47] text-center space-y-4 my-10">
-        <img src={blok.Servicesmain_Image4.filename} alt="Service Main 4" className="max-w-full h-auto"/>
-        <h2 className="text-[#EAFFBD] text-lg">
-          {blok.Servicesmain_Titel4}
-        </h2>
-        <p className="text-white text-sm">{blok.Servicesmain_Tekst4}</p>
+      <div className="onder flex">
+        {/*3*/}
+        <div className="w-[50%] bg-[#002626]">
+          <div className="bg-[#002626] flex flex-col justify-between text-left p-10 space-y-4">
+            <div className="flex justify-center">
+              <img src={blok.Servicesmain_Image3.filename} alt="Service Main 1" className="w-[72%] h-auto"/>
+            </div>
+            <div className=" w-[90%] flex flex-col items-center">
+              <div className="flex justify-start w-[70%]">
+                <h2 className=" text-[#EAFFBD] text-3xl mb-2 font-bold">{blok.Servicesmain_Titel3}</h2>
+              </div>
+                <div className="ml-2 w-[72%]">
+                  <p className=" text-white text-sm">{blok.Servicesmain_Tekst3}</p>
+                </div>
+                <div className="flex justify-start w-[70%] mt-4">
+                  <button className="bg-[#F4C5FF] px-6 py-2 rounded-2xl">Discover</button>
+                </div>
+            </div>
+          </div>
+        </div>
+        {/*4*/}
+        <div className="w-[50%] bg-[#003a47]">
+          <div className="bg-[#003A47] flex flex-col justify-between text-left p-10 space-y-4">
+            <div className="flex justify-center">
+              <img src={blok.Servicesmain_Image4.filename} alt="Service Main 1" className="w-[72%] h-auto"/>
+            </div>
+            <div className=" w-[90%] flex flex-col items-center">
+              <div className="flex justify-start w-[70%]">
+                <h2 className=" text-[#EAFFBD] text-3xl mb-2 font-bold">{blok.Servicesmain_Titel4}</h2>
+              </div>
+                <div className="ml-2 w-[72%]">
+                  <p className=" text-white text-sm">{blok.Servicesmain_Tekst4}</p>
+                </div>
+                <div className="flex justify-start w-[70%] mt-4">
+                  <button className="bg-[#F4C5FF] px-6 py-2 rounded-2xl">Discover</button>
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
