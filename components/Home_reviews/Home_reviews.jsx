622f661119bfc70de6c19665457513aebe58cec6
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2024-12-13 12:32:59 +0100

    review component gemaakt

---
+++ components/Home_reviews/Home_reviews.jsx
@@ -0,0 +1,19 @@
+import { storyblokEditable } from "@storyblok/react/rsc";
+import { render } from "storyblok-rich-text-react-renderer";
+ 
+const Home_reviews = ({ blok }) => (
+  <div className="bg-[#002626] my-10 flex flex-col justify-center items-center" {...storyblokEditable(blok)}>
+    <div className="text-[#A8FF1A] mb-4"  style={{
+                fontFamily: "Area, sans-serif",
+                fontWeight: "500",
+                fontSize: "60px",
+              }}>
+      {render(blok.review_header)}
+    </div>
+
+    <img className="" src={blok.revieuw_image.filename} />
+  </div>
+);
+ 
+ 
+export default Home_reviews;
